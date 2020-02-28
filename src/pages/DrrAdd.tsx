0cd274b7018e2a1f095a9fb95a13c4429ecd5fb6
qunqun.xu 许群群 <qunqun.xu@qiyuesuo.com>
2020-02-28 17:24:23 +0800

    react map key, 循环时每个组件标识key, fix useState循环时数组覆盖的问题

--- src/pages/DrrAdd.tsx
+++ src/pages/DrrAdd.tsx
@@ -17,115 +17,138 @@ export interface DrrData {
   pageIndex: number;
 }
 
-const Index: React.FC = props => {
-
-  let aRef = React.createRef();
-
-  let [drrs, setDrrs] = useState([]);
-
-  const [pageIndex, setPageIndex] = useState(0)
-
-  const prev = () => {
-    let i = pageIndex - 1
-    if (i < 0) {
-      i = 0
-    }
-    setPageIndex(i)
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      drrs: [],
+      pageIndex: 0
+    };
   }
 
-  const next = () => {
-    let i = pageIndex + 1
-    if (i >= aRef.pages.length) {
-      i = aRef.pages.length - 1
-    }
-    setPageIndex(i)
-  }
+  render() {
+    const { drrs, pageIndex } = this.state
+    let aRef = null
 
-  const add = () => {
-    const item: DrrData = {
-      src: 'favicon.png',
-      left: 0,
-      top: 0,
-      width: 64,
-      height: 64,
-      rotateAngle: 0,
-      pageIndex: pageIndex,
-      handleDrag: (deltaX: number, deltaY: number) => {
-        item.top += deltaY
-        item.left += deltaX
-        setDrrs([...drrs])
-      },
-      handleResize: (style, isShiftKey, type) => {
-        let { top, left, width, height } = style
-        item.top = Math.round(top)
-        item.left = Math.round(left)
-        item.width = Math.round(width)
-        item.height = Math.round(height)
-        setDrrs([...drrs])
-      },
-      handleRotate: (rotateAngle: number) => {
-        item.rotateAngle = rotateAngle
-        setDrrs([...drrs])
+    const prev = () => {
+      const { pageIndex } = this.state
+      let i = pageIndex - 1
+      if (i < 0) {
+        i = 0
       }
+      this.setState(state => {
+        return {
+          ...state, pageIndex: i
+        }
+      })
     }
-    drrs.push(item)
-    setDrrs([...drrs])
+
+    const next = () => {
+      const { pageIndex } = this.state
+      let i = pageIndex + 1
+      if (aRef && i >= aRef.pages.length) { // ref, 不超出最后一页
+        i = aRef.pages.length - 1
+      }
+      this.setState(state => {
+        return {
+          ...state, pageIndex: i
+        }
+      })
+    }
+
+    const add = () => {
+      let left = 0, top = 0
+      if (drrs && drrs.length > 0) {
+        left = drrs[drrs.length - 1].left + 24
+        top = drrs[drrs.length - 1].top + 24
+      }
+
+      const item: DrrData = {
+        src: 'favicon.png',
+        left: left,
+        top: top,
+        width: 64,
+        height: 64,
+        rotateAngle: 0,
+        pageIndex: pageIndex,
+        handleDrag: (deltaX: number, deltaY: number) => {
+          item.top += deltaY
+          item.left += deltaX
+          this.setState(state => {
+            return { ...state }
+          })
+        },
+        handleResize: (style, isShiftKey, type) => {
+          let { top, left, width, height } = style
+          item.top = Math.round(top)
+          item.left = Math.round(left)
+          item.width = Math.round(width)
+          item.height = Math.round(height)
+          this.setState(state => {
+            return { ...state }
+          })
+        },
+        handleRotate: (rotateAngle: number) => {
+          item.rotateAngle = rotateAngle
+          this.setState(state => {
+            return { ...state }
+          })
+        }
+      }
+      drrs.push(item)
+      this.setState(state => {
+        return { ...state }
+      })
+    }
+    return (
+      <>
+        <Button onClick={prev}>上一页</Button>
+        <Button onClick={next}>下一页</Button>
+        <Button onClick={add}>add</Button>
+
+        <hr />
+
+        <Document file="e9.pdf" ref={ele => aRef = ele}>
+          <Page pageIndex={pageIndex}
+            renderMode="svg"
+            // todo, 文本层渲染x方向有偏差
+            renderTextLayer={false}>
+            {
+              drrs && drrs.map((item: DrrData, key) => {
+                if (pageIndex == item.pageIndex) {
+                  return (
+                    <Drr
+                      key={`show-${key}`}
+                      left={item.left}
+                      top={item.top}
+                      width={item.width}
+                      height={item.height}
+                      rotateAngle={item.rotateAngle}
+                      // aspectRatio={false}
+                      // zoomable='n, w, s, e'
+                      zoomable='n, w, s, e, nw, ne, se, sw'
+                      // rotatable={true}
+                      onRotate={item.handleRotate}
+                      onResize={item.handleResize}
+                      onDrag={item.handleDrag}
+                    >
+                      <img src={item.src} style={{
+                        width: item.width,
+                        height: item.height,
+                        userSelect: "none",
+                        pointerEvents: "none"
+                      }} />
+                    </Drr>
+                  )
+                }
+                return (<div key={`not-show-${key}`}></div>)
+              })
+            }
+          </Page>
+        </Document>
+      </>
+    )
   }
-
-  return (
-    <>
-      <Button onClick={() => {
-        prev()
-      }}>上一页</Button>
-      <Button onClick={() => { next() }}>下一页</Button>
-      <Button onClick={() => { add() }}>add</Button>
-
-      <hr />
-
-      <Document file="e9.pdf" ref={(ele) => {
-        aRef = ele;
-      }} >
-        <Page pageIndex={pageIndex}
-          renderMode="svg"
-          // todo, 文本层渲染x方向有偏差
-          renderTextLayer={false}>
-          {
-            drrs && drrs.map((item: DrrData, key) => {
-              if (pageIndex == item.pageIndex) {
-                return (
-                  <Drr
-                    key={key}
-                    left={item.left}
-                    top={item.top}
-                    width={item.width}
-                    height={item.height}
-                    rotateAngle={item.rotateAngle}
-                    // aspectRatio={false}
-                    minWidth={20}
-                    minHeight={20}
-                    zoomable='n, w, s, e'
-                    // zoomable='n, w, s, e, nw, ne, se, sw'
-                    // rotatable={true}
-                    onRotate={item.handleRotate}
-                    onResize={item.handleResize}
-                    onDrag={item.handleDrag}
-                  >
-                    <img src={item.src} style={{
-                      width: item.width,
-                      height: item.height,
-                      userSelect: "none",
-                      pointerEvents: "none"
-                    }} />
-                  </Drr>
-                )
-              }
-              return (<></>)
-            })
-          }
-        </Page>
-      </Document>
-    </>
-  )
 }
 
 export default Index;
