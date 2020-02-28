7cd2fa603fa7f5e27ae2ff315398961bf0955e5c
qunqun.xu 许群群 <qunqun.xu@qiyuesuo.com>
2020-02-28 19:58:38 +0800

    计算矩形四角偏移量

--- src/pages/DrrAdd2.tsx
+++ src/pages/DrrAdd2.tsx
@@ -54,6 +54,7 @@ class Index extends React.Component {
           this.setState(state => {
             return { ...state }
           })
+          f4()
         },
         handleResize: (style, isShiftKey, type) => {
           let { top, left, width, height } = style
@@ -65,6 +66,7 @@ class Index extends React.Component {
           this.setState(state => {
             return { ...state }
           })
+          f4()
         },
         handleRotate: (rotateAngle: number) => {
           item.rotateAngle = rotateAngle
@@ -72,18 +74,63 @@ class Index extends React.Component {
           this.setState(state => {
             return { ...state }
           })
+          f4()
         }
       }
       drrs.push(item)
       this.setState(state => {
-        console.log(state)
         return { ...state }
       })
+      f4()
+    }
+
+    const f4 = () => {
+      const item: DrrData = drrs[0]
+      if (!item) {
+        return
+      }
+
+      const r = Math.sqrt(Math.pow(item.width / 2, 2) + Math.pow(item.height / 2, 2))
+      const rotateAngle = item.rotateAngle
+      console.log(`item=>${JSON.stringify(item)}, r=>${r}, `)
+
+      // 以下开始计算四角偏移量
+      // 中心点
+      const x = item.left + item.width / 2
+      const y = item.top + item.height / 2
+      // 弧度
+      const a = Math.PI / 180 * item.rotateAngle
+      // 
+      const a4 = Math.atan(item.height / item.width)
+      const aa4 = a4 + a
+      const dx4 = Math.cos(aa4) * r
+      const dy4 = Math.sin(aa4) * r
+
+      // 
+      const a3 = Math.atan(item.width / item.height)
+      const aa3 = a3 + Math.PI / 2 + a
+      const dx3 = Math.cos(aa3) * r
+      const dy3 = Math.sin(aa3) * r
+
+      //
+      const dx2 = -dx4
+      const dy2 = -dy4
+
+      //
+      const dx1 = -dx3
+      const dy1 = -dy3
+
+      const x4 = Math.round(dx4 + x), x3 = Math.round(dx3 + x), x2 = Math.round(dx2 + x), x1 = Math.round(dx1 + x)
+      const y4 = Math.round(dy4 + y), y3 = Math.round(dy3 + y), y2 = Math.round(dy2 + y), y1 = Math.round(dy1 + y)
+
+      console.log(`x=>${x}, y=>${y}, dx4=>${dx4}, dy4=>${dy4}, dx3=>${dx3}, dy3=>${dy3}, dx2=>${dx2}, dy2=>${dy2}, dx1=>${dx1}, dy1=>${dy1}, `)
+      console.log(`x4=>${x4}, y4=>${y4}, x3=>${x3}, y3=>${y3}, x2=>${x2}, y2=>${y2}, x1=>${x1}, y1=>${y1}, `)
     }
 
     return (
       <div>
         <Button onClick={add}>add</Button>
+        <Button onClick={f4}>f4</Button>
         <hr />
         <div style={{ display: 'block', position: 'relative' }}>
           {
